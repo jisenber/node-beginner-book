d83726cb752fdd1cc317de554137f47e86fdb16a
jisenber <isenberg.jacob@yahoo.com>
2016-11-27 21:26:47 -0800

    finished beginner node book

--- book-proj/requestHandlers.js
+++ book-proj/requestHandlers.js
@@ -1,15 +1,20 @@
+var fs = require('fs');
+var formidable = require('formidable');
+//require the core filesystem module and the formidable npm node module.
+
 function start(res) {
   console.log ('request handler \'start\' was called.');
 
   var body = '<html>' +
   '<head>' +
-  '<meta http-equiv="content-Type" content = "text/html; ' +
-  'charset=UTF-8 />' +
+  '<meta http-equiv="Content-Type" ' +
+  'content="text/html; charset=UTF-8" />' +
   '</head>' +
   '<body>' +
-  '<form action = "/upload" method = "post">' +
-  '<textarea name = "text" rows = "20" cols ="60"> </textarea>' +
-  '<input type="submit" value = "submit text" />' +
+  '<form action="/upload" enctype="multipart/form-data" ' +
+  'method="post">' +
+  '<input type="file" name="upload" multiple="multiple">' +
+  '<input type="submit" value="Upload file" />' +
   '</form>' +
   '</body>' +
   '</html>';
@@ -17,17 +22,43 @@ function start(res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(body);
   res.end();
+
+  //start function that takes a response object. that started in server.js. this is just html assigned to a variable that is passed into the .write method for the response object.
 }
 
-function upload(res) {
+function upload(res, req) {
   console.log('request handler \'upload\' was called');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello Upload');
-  res.end();
+
+  var form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(req, function(error, fields, files) {
+    console.log('parsing done');
+
+    fs.rename(files.upload.path, '/tmp/test.png', function(error) {
+      if (error) {
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
+    });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('received image: <br/>');
+    res.write("<img src='/show' />");
+    res.end();
+  });
 }
+//this is the picture uploading function. It handles the form that is submitted by the user. The submitted form is a png picture. This function parses the picture submitted by the user and then injects html into the response object that will show the picture by taking the user to the /show route.
+
+function show(res) {
+  console.log('request handler \'show\' was called');
+  res.writeHead(200, {'Content-Type': 'image/png'});
+  fs.createReadStream('/tmp/test.png').pipe(res);
+}
+
+//this is the show function that makes the response header expect a pong image and pipes the data from the /tmp/test.png local folder (which is populated by the upload function) to the response object.
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
 
 //this allows us to wire the request handlers into the router, giving our router something to route to.
 
--- book-proj/router.js
+++ book-proj/router.js
@@ -1,8 +1,7 @@
-function route(handle, pathname, res) {
+function route(handle, pathname, res, req) {
   console.log('about to route a request for ' + pathname);
   if (typeof handle[pathname] === 'function') {
-    handle[pathname](res);
-    //instead of expecting a return value from our request handlers, we pass the res object on
+    handle[pathname](res, req);
   } else {
     console.log('no request handler found for ' + pathname);
     res.writeHead(404, {'Content-Type': 'text/plain'});
@@ -11,8 +10,10 @@ function route(handle, pathname, res) {
   }
 }
 
-//handle is defined in index.js and the pathname is defined in server.js. index.js makes handle an empty object at first, then assigns it properties that function as routes. The value pair to those routes is a function that fires.
+//routing function is define here. It takes 4 parameters: Handle, pathname, response and request. Pathname is defined in server.js (url path). handle is defined in index.js. Request and response data are objects created on the server request and passed from server js to this file and passed on again.
+
+//handle is created in index.js first as an empty object, with added properties that are the routes we will be using. The value of each property is a function so that each route can in effect trigger a function call. if the property value of the handle object is a function, invoke that function with out response and request parameters. Otherwise give a 404 response.
 
 exports.route = route;
 
-//routing 'ends' in the router and the router is not actually he place to 'do' something complex, becasue that wouldn't scale well.
+//export the route function
